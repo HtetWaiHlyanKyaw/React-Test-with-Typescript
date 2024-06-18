f48a9ab5f10d3f750ad3594c7bf413f1b02de876
Jaz <htetwaihlyankyaw27@gmail.com>
2024-06-18 23:53:04 +0630

    First Commit

---
+++ src/Message.tsx
@@ -0,0 +1,8 @@
+function Message(){
+    const name = "Htet Wai";
+    if(name === "Htet Wai"){
+        return <h1>Hello, {name}</h1>;
+    }
+    return <h1>Hello World</h1>;
+}
+export default Message;
