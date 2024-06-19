3fcd996bb2b9e232f2cc6329099394f74751d6de
Jaz <htetwaihlyankyaw27@gmail.com>
2024-06-19 12:23:27 +0630

    Finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,43 @@
 // import React from "react";
-import Message from './Message';
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+import ListGroup from "./components/ListGroup";
+import { useState } from "react";
 
 function App() {
-  return <div><Message/></div>;
+  let items = ["New York", "London", "Paris", "Tokyo"];
+  let gays = ["Aung Aung", "Maung Maung", "Hla Hla", "Win Win"];
+  const handleSelectedItem = (item: string) => {
+    console.log(item);
+  };
+  const [alertVisible, setAlertVisible] = useState(false);
+  return (
+    <>
+      <div>
+        <ListGroup
+          items={items}
+          heading="Cities"
+          onSelectedItem={handleSelectedItem}
+        />
+      </div>
+
+      <div>
+        <ListGroup
+          items={gays}
+          heading="Gays"
+          onSelectedItem={handleSelectedItem}
+        />
+      </div>
+
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisible(false)}>My Alert</Alert>
+      )}
+
+      <Button color="primary" onClick={() => setAlertVisible(true)}>
+        My Button
+      </Button>
+    </>
+  );
 }
 
 export default App;
